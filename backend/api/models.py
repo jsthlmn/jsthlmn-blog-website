18ec29422305579e914c3bc0529abf6a7440e64f
jsthlmn <hpanser@gmail.com>
2025-01-09 21:04:06 +0700

    Custom user profile

--- backend/api/models.py
+++ backend/api/models.py
@@ -46,3 +46,15 @@ class Profile(models.Model):
         if self.full_name == "" or self.full_name == None:
             self.full_name = self.user.full_name
         super(Profile, self).save(*args, **kwargs)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
